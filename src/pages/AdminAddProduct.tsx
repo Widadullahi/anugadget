5bbfa4f5ee7a3d5cfc713195a5fbba45bceee21e
Widadullahi <alagbewidad814@gmail.com>
2026-02-07 14:50:34 +0100

    Edited

--- src/pages/AdminAddProduct.tsx
+++ src/pages/AdminAddProduct.tsx
@@ -12,10 +12,7 @@ const productCategories = [
   "Wearables",
   "Audio",
   "Gaming",
-  "Accessories",
-  "Appliances",
-  "Power Solutions",
-  "Smart Home"
+  "Accessories"
 ];
 
 const loadProducts = () => {
